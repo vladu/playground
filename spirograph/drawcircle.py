a75d8b35653982542cf1528a05c8e7fbf5adad41
vladu <vovansim@gmail.com>
2021-03-02 14:09:26 -0600

    First stab at draw spiro implementation

--- spirograph/drawcircle.py
+++ spirograph/drawcircle.py
@@ -2,23 +2,35 @@ import numpy
 import turtle
 
 
-def draw_points(x0, y0, points):
+def draw_points(points):
     turtle.penup()
-    turtle.goto(x0, y0)
+    turtle.goto(points[0][0], points[0][1])
     turtle.pendown()
 
     for p in points:
         turtle.goto(p[0], p[1])
 
 
+def draw_spiro(x0, y0, outer_r, inner_r, pen_r_pct, rad_incr=0.05):
+    k = inner_r / outer_r
+
+    phi = numpy.arange(0, 2 * numpy.pi * inner_r / numpy.gcd(inner_r, outer_r), rad_incr)
+    points = numpy.asarray([
+        x0 + outer_r * ((1 - k) * numpy.cos(phi) + pen_r_pct * k * numpy.cos(phi * (1 - k) / k)),
+        y0 + outer_r * ((1 - k) * numpy.sin(phi) + pen_r_pct * k * numpy.sin(phi * (1 - k) / k)),
+    ]).T
+    draw_points(points)
+
+
 def draw_circle(x, y, r):
     phi = numpy.arange(0, 2 * numpy.pi, 0.05)
     points = numpy.asarray([x + r * numpy.cos(phi), y + r * numpy.sin(phi)]).T
-    draw_points(x + r, y, points)
+    draw_points(points)
 
 
 def main():
-    draw_circle(100, 100, 50)
+    # draw_circle(100, 100, 50)
+    draw_spiro(50, 50, 220, 65, 0.8)
     turtle.mainloop()
 
 
