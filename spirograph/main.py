fb160d2cd503fdc9ed42f3dc136104021c75bbdc
vladu <vovansim@gmail.com>
2021-03-02 14:37:41 -0600

    Refactor draw_points, so that it can run as a generator, so we can draw multiple spiros at the same time.

--- spirograph/main.py
+++ spirograph/main.py
@@ -2,16 +2,26 @@ import numpy
 import turtle as turtle_mod
 
 
-def draw_points(points):
+def draw_points_gen(points):
     turtle = turtle_mod.Turtle()
-    turtle.penup()
-    turtle.goto(points[0][0], points[0][1])
-    turtle.pendown()
 
-    for p in points:
-        turtle.goto(p[0], p[1])
+    for idx, p in enumerate(points):
+        if idx == 0:
+            turtle.penup()
+            turtle.goto(p[0], p[1])
+            turtle.pendown()
+        else:
+            turtle.goto(p[0], p[1])
+        yield idx
 
     turtle.hideturtle()
+
+
+def draw_points(points):
+    cnt = 0
+    for cnt in draw_points_gen(points):
+        pass
+    return cnt
 
 
 def draw_spiro(x0, y0, outer_r, inner_r, pen_r_pct, rad_incr=0.05):
@@ -22,18 +32,18 @@ def draw_spiro(x0, y0, outer_r, inner_r, pen_r_pct, rad_incr=0.05):
         x0 + outer_r * ((1 - k) * numpy.cos(phi) + pen_r_pct * k * numpy.cos(phi * (1 - k) / k)),
         y0 + outer_r * ((1 - k) * numpy.sin(phi) + pen_r_pct * k * numpy.sin(phi * (1 - k) / k)),
     ]).T
-    draw_points(points)
+    return draw_points(points)
 
 
 def draw_circle(x, y, r):
     phi = numpy.arange(0, 2 * numpy.pi, 0.05)
     points = numpy.asarray([x + r * numpy.cos(phi), y + r * numpy.sin(phi)]).T
-    draw_points(points)
+    return draw_points(points)
 
 
 def main():
-    draw_circle(100, 100, 50)
-    draw_spiro(50, 50, 220, 65, 0.8)
+    print("Circle has {} points".format(draw_circle(100, 100, 50)))
+    print("Spiro has {} points".format(draw_spiro(50, 50, 220, 65, 0.8)))
     turtle_mod.mainloop()
 
 
