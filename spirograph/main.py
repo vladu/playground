fd64769765df9da1adbcf3a1d444a992b3b82393
vladu <vovansim@gmail.com>
2021-03-02 15:14:06 -0600

    Drawing multiple curves at the same time now works.

--- spirograph/main.py
+++ spirograph/main.py
@@ -1,8 +1,10 @@
+import collections
+
 import numpy
 import turtle as turtle_mod
 
 
-def _draw_points_gen(points):
+def _draw_curve_async(points):
     turtle = turtle_mod.Turtle()
 
     for idx, p in enumerate(points):
@@ -17,33 +19,50 @@ def _draw_points_gen(points):
     turtle.hideturtle()
 
 
-def draw_points(points):
+def draw_curve(points):
     cnt = 0
-    for cnt in _draw_points_gen(points):
+    for cnt in _draw_curve_async(points):
         pass
     return cnt
 
 
-def draw_spiro(x0, y0, outer_r, inner_r, pen_r_pct, rad_incr=0.05):
+def draw_curves(curves, t=10):
+    all_gens = collections.OrderedDict((_draw_curve_async(points), 0) for points in curves)
+    done_gens = set()
+
+    def one_step():
+        if len(done_gens) < len(all_gens):
+            for gen in all_gens.keys():
+                if gen not in done_gens:
+                    try:
+                        all_gens[gen] = next(gen)
+                    except StopIteration:
+                        done_gens.add(gen)
+            turtle_mod.ontimer(one_step, t)
+
+    turtle_mod.ontimer(one_step, t)
+
+
+def calc_spiro(x0, y0, outer_r, inner_r, pen_r_pct, rad_incr=0.05):
     k = inner_r / outer_r
 
     phi = numpy.arange(0, 2 * numpy.pi * inner_r / numpy.gcd(inner_r, outer_r), rad_incr)
-    points = numpy.asarray([
+    return numpy.asarray([
         x0 + outer_r * ((1 - k) * numpy.cos(phi) + pen_r_pct * k * numpy.cos(phi * (1 - k) / k)),
         y0 + outer_r * ((1 - k) * numpy.sin(phi) + pen_r_pct * k * numpy.sin(phi * (1 - k) / k)),
     ]).T
-    return draw_points(points)
 
 
-def draw_circle(x, y, r):
+def calc_circle(x, y, r):
     phi = numpy.arange(0, 2 * numpy.pi, 0.05)
-    points = numpy.asarray([x + r * numpy.cos(phi), y + r * numpy.sin(phi)]).T
-    return draw_points(points)
+    return numpy.asarray([x + r * numpy.cos(phi), y + r * numpy.sin(phi)]).T
 
 
 def main():
-    print("Circle has {} points".format(draw_circle(100, 100, 50)))
-    print("Spiro has {} points".format(draw_spiro(50, 50, 220, 65, 0.8)))
+    draw_curves([
+        calc_circle(100, 100, 50),
+        calc_spiro(50, 50, 220, 65, 0.8),
+    ])
     turtle_mod.mainloop()
 
 
